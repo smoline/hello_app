d723bf85fe238a27994c44efdd52da2e6d6ef042
Sherilyn Moline <smoline@gmail.com>
2017-07-02 10:12:35 -0400

    Rails Tutorial changing the routes and content of the first page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'application#goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
